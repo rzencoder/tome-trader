98e21c6e669d7bb614bfc3a976fefebd79b68118
adam <hamer88@hotmail.co.uk>
2017-09-29 12:43:02 +0100

    heroku deploy

--- public/bundle.js
+++ public/bundle.js
@@ -92,7 +92,7 @@ var _reactRouter = __webpack_require__("./node_modules/react-router/es/index.js"
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //Requirements
-var path = 'https://tometrader.herokuapp.com/';
+var path = 'https://tometrader.herokuapp.com';
 
 var LOGIN_SUCCESS = exports.LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 var LOGIN_FAILURE = exports.LOGIN_FAILURE = 'LOGIN_FAILURE';
@@ -1809,7 +1809,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var BASE_URL = 'https://tometrader.herokuapp.com/';
+var BASE_URL = 'https://tometrader.herokuapp.com';
 
 function callApi(endpoint, authenticated) {
 
